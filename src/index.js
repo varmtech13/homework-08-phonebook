de1b09a441f6a86a7fe3945091e61468960a7875
varmtech13 <varmtech.co@gmail.com>
2022-11-25 14:28:28 +0100

    Adding routes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/homework-08-phonebook/">
           <ChakraProvider>
             <CSSReset />
             <App />
